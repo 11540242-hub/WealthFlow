b579c69375d7e09b91c9e47cb12234529adead32
11540242-hub <11540242@me.mcu.edu.tw>
2025-12-11 18:56:39 +0800

    chore: Update dependencies and deployment config
    Updates the `@google/genai` package to version `0.2.0` and refactors the Firebase initialization to use the `firebaseApp` namespace. Also removes the outdated GitHub Actions deploy workflow as it's no longer needed.

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import * as firebaseApp from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // These variables will be injected by Vite from GitHub Secrets/Environment variables
@@ -12,5 +12,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebaseApp.initializeApp(firebaseConfig);
 export const db = getFirestore(app);
